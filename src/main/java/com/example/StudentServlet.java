b621d9f0d16cd18f07f3def9865a50fab319b811
mmbabu0705 <mmbabu0705@gmail.com>
2026-01-07 12:53:42 +0530

    Update StudentServlet.java

--- src/main/java/com/example/StudentServlet.java
+++ src/main/java/com/example/StudentServlet.java
@@ -1,3 +1,13 @@
+package com.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/register")
 public class StudentServlet extends HttpServlet {
 
@@ -10,7 +20,6 @@ public class StudentServlet extends HttpServlet {
         String course = req.getParameter("course");
         String phone = req.getParameter("phone");
 
-        // Send form values to success.jsp
         req.setAttribute("name", name);
         req.setAttribute("email", email);
         req.setAttribute("course", course);
